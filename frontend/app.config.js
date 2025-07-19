71d0e38de3fb9ecf3a74ee41a21b56fa22c0e4ae
caljun <calderonjunya0602@gmail.com>
2025-07-19 21:53:17 +0900

    Update app.config.js

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -20,6 +20,7 @@ export default {
     assetBundlePatterns: ['**/*'],
     ios: {
       supportsTablet: true,
+      bundleIdentifier: "com.yourapp.areaapp",
     },
     android: {
       adaptiveIcon: {
