39ba56a338c9aa919271cd6c0dcb0d3f963d17fe
caljun <calderonjunya0602@gmail.com>
2025-07-20 18:26:38 +0900

    g

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -3,7 +3,6 @@ const { createMetroConfiguration } = require('@expo/metro-config');
 
 const config = createMetroConfiguration(__dirname);
 
-// 必要に応じてカスタマイズ
 config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 config.transformer.minifierConfig = {
   keep_fnames: true,
