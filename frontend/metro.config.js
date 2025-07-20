3c1abc6256b3de16380271599853c5f57ab5cfc1
caljun <calderonjunya0602@gmail.com>
2025-07-20 18:23:02 +0900

    Update metro.config.js

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,15 +1,16 @@
 // metro.config.js
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
+module.exports = (async () => {
+  const config = await getDefaultConfig(__dirname);
 
-// 必要に応じて設定を追加
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-config.transformer.minifierConfig = {
-  keep_fnames: true,
-  mangle: {
+  config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+  config.transformer.minifierConfig = {
     keep_fnames: true,
-  },
-};
+    mangle: {
+      keep_fnames: true,
+    },
+  };
 
-module.exports = config;
+  return config;
+})();
