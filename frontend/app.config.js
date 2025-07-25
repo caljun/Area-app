3264a54b29863ab14327d66831ed4a508b1b9e9d
caljun <calderonjunya0602@gmail.com>
2025-07-26 00:15:52 +0900

    Update app.config.js

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -25,7 +25,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.yourapp.areaapp",
-      buildNumber: "15",
+      buildNumber: "16",
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false
       }
